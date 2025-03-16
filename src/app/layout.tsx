ae688a4e387d407cb87f5939b65fc05a3b89d743
Abhishek Shetty <abhivishu99@gmail.com>
2025-03-16 23:32:44 +0530

    chore: changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,18 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <head><!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M23NFP7L');</script>
-        <!-- End Google Tag Manager --></head>
       <body>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M23NFP7L"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         {children}
         <Toaster />
       </body>
